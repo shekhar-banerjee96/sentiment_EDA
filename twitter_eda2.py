4f2ff7e2e7d888d3b4f173bfc065093b2512ab82
Banerjee1996 <shekhar.banerjee96@gmail.com>
2023-05-08 16:15:31 +0530

    fix

--- twitter_eda2.py
+++ twitter_eda2.py
@@ -23,7 +23,7 @@ import time
 nltk.download('punkt')
 nltk.download('stopwords')
 sns.color_palette("cubehelix", as_cmap=True)
-sns.set_theme(style="darkgrid", palette="pastel")
+#sns.set_theme(style="darkgrid", palette="pastel")
 stop_words = set(stopwords.words('english'))
 geolocator = Nominatim(user_agent = "geoapiExercises")
 ps = PorterStemmer()
