10d55496f939e5df80319037a05892cf0e18cb53
Banerjee1996 <shekhar.banerjee96@gmail.com>
2023-05-09 00:08:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns 
+import seaborn as sns
 
+fig , ax = plt.subplots()
 df = sns.load_dataset("penguins")
-fig,_ = sns.barplot(data=df, x="island", y="body_mass_g")
+fig,_ = sns.barplot(data=df, x="island", y="body_mass_g",ax=ax)
 
 st.pyplot(fig)
