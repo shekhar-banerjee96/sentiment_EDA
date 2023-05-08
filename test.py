1c70c4588d7aacaeab66ce10f6c08884c91ec8b9
Banerjee1996 <shekhar.banerjee96@gmail.com>
2023-05-08 23:52:06 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns 
+
+df = sns.load_dataset("penguins")
+sns.barplot(data=df, x="island", y="body_mass_g")
+
+st.pyplot(fig)
